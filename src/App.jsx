6cbd57f552a25ea474b431f5ca2bb50cac206bd4
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-06-24 16:07:35 +0100

    Values.js setup for list more colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react"
 import ColorList from "./ColorList"
 import Form from "./Form"
+import Values from "values.js"
 
 const App = () => {
+  const [colors, setColors] = useState()
+  // This show different list of colors relating to the input. from light to dark
+  console.log(new Values("#0000").all(10))
+
   return (
     <section>
       <Form />
