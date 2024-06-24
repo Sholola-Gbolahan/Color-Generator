7220ee0610cf0221d930a06b307cf406912dfd1e
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-06-24 17:06:05 +0100

    Toastify Setup used

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from "react"
 import ColorList from "./ColorList"
 import Form from "./Form"
 import Values from "values.js"
+import { ToastContainer, toast } from "react-toastify"
 
 const App = () => {
   const [colors, setColors] = useState(new Values("#1f45").all(10))
 
-  // This show different list of colors relating to the input. from light to dark
-
+  toast.error("Error")
+  toast.success("success")
   return (
     <section>
       <Form />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </section>
   )
 }
